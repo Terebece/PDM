3dd38ab189688fab91b7404f4ddf23df2335540a
Teresa Becerril <terebece1508@ciencias.unam.mx>
2021-08-09 15:11:36 -0500

    Agregando Tarea 1

---
+++ Tareas/Tarea1/app/src/main/java/com/example/teresabecerril/tarea1/Tarea1.java
@@ -0,0 +1,17 @@
+package com.example.teresabecerril.tarea1;
+
+/**
+ * Created by Teresa Becerril on 08/Agosto/2021.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tarea1 extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
